90acabf73c060feed178243fa94b2841ef86fefd
Wiam Nasr <wiamnasr@gmail.com>
2022-01-10 11:39:12 +0000

    added first subscription to my react-redux store that is managed by react-redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,18 @@
 // Importing a custom hook made by the React Redux team
+// note, there is also a useStore hook that can be used as well, that gives direct access to the store
+// But useSelector is a bit more convenient to use, because that allows me to automatically select a part of the state managed by the store
+// Note: if a class based component was being used, then there is also a connect function that can be used as a wrapper around the class component, to connect that class component to the store
+import { useSelector } from "react-redux";
 
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // Now there is access to the data managed by the store, by using 'useSelector()'
+  // a function must be passed, it will be executed by react-redux, this function determines which piece of data we want to extract from the store
+  // Important =>using useSelector, react-redux will automatically setup a subscription to the redux store for this component
+  // this subscription is managed by react-redux, meaning if the component was removed (unmounted for some reason), react-redux will automatically unsubscribe it from the store => handy!
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
